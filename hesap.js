c933c40743839d337a9c3646b8b3ad7c53e46ba0
omerfarkkekec <omerfarkkekec@gmail.com>
2025-12-19 23:08:01 +0300

    Add files via upload

--- hesap.js
+++ hesap.js
@@ -1,6 +1,9 @@
 let inputBox = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 let sound = document.getElementById('clickSound');
+let historyList = document.getElementById('historyList');
+let clearHistoryBtn = document.getElementById('clearHistory');
+
 
 let string ='';
 
@@ -11,9 +14,20 @@ buttons.forEach( element => {
         sound.play();
 
         if(b.target.innerText == '=') {
+            let eskiString = string;
             string= String(eval(string));
             inputBox.value = string;
-        }
+
+            let li = document.createElement('li');
+            li.innerText = `${eskiString} = ${string}`;
+    
+            let sonuc = string;
+            li.onclick = () => {
+               string = sonuc; 
+               inputBox.value = sonuc;
+            };
+        historyList.prepend(li); // En yeni işlemi en üste ekle
+        } 
         else if(b.target.innerText == 'AC') {
             string = '';
             inputBox.value = string;
@@ -27,8 +41,13 @@ buttons.forEach( element => {
             inputBox.value = string;
         }
         else{
+            if(b.target.id !== 'clearHistory'){
             string += b.target.innerText;
             inputBox.value = string;
+            }
         }
     })
 })
+clearHistoryBtn.onclick = () => {
+    historyList.innerHTML = '';
+};
